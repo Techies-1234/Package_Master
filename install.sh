f23e0feefe53db1b081fd4c6f87a8306a63a75f2
World Of Techies <98240373+Techies-1234@users.noreply.github.com>
2022-02-03 18:55:42 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,125 @@
+#!/data/data/com.termux/files/usr/bash
+
+echo $red 
+
+ echo ""----------------------------------------------------------------------------- ""
+
+echo " This Script is Made by ██ 𝐄𝐑𝐑☢︎𝐑-𝐗-𝐏𝐑☢︎𝐅𝐄𝐒𝐒☢︎𝐑⁩ ██"
+
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo  "#####################################"
+
+echo   "  https://github.com/Techies-1234     "               
+
+ echo   " Learn Eathical Hacking Free"                                                                    
+
+echo   "####################################"
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo "."
+
+apt update -y                      #update in progress
+
+apt install python -y           #installation......
+
+apt install python2 -y
+
+apt install ruby -y 
+
+apt install git -y
+
+echo  "#####################################"
+
+ echo   " https://github.com/Techies-1234 "                                                                    
+
+echo   "####################################"
+
+apt install php -y 
+
+apt install perl -y
+
+apt install nmap -y
+
+echo  "#####################################"
+
+echo   "  GO TO MY WEBSITE AND LEARN HACKING  "             
+
+ echo   " https://github.com/Techies-1234 "                                                                    
+
+echo   "####################################"
+
+apt install bash -y
+
+apt install clang -y
+
+apt install jq -y
+
+apt install macchanger -y
+
+echo  "#####################################"               
+
+ echo   " EATHICAL HACKING "                                                                    
+
+echo   "####################################"
+
+apt install nano -y
+
+apt install curl -y
+
+apt install tar -y
+
+apt install zip -y
+
+echo  "#####################################"
+
+echo   " MY WORDPRESS BLOG     "               
+
+ echo   " https://github.com/Techies-1234 "                                                                    
+
+echo   "####################################"
+
+apt install unzip -y
+
+apt install tor -y
+
+apt install wget -y
+
+echo  "#####################################"
+
+echo   "  https://github.com/Techies-1234      "               
+
+ echo   " EATHICAL HACKING "                                                                    
+
+echo   "####################################"
+
+apt install wcalc -y 
+
+apt install openssl
+
+apt install bmon -y
+
+echo  "#####################################"
+
+echo   "  GO TO MY WEBSITE      "               
+
+ echo   " https://github.com/Techies-1234"                                                                    
+
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+echo "𝓐𝓵𝓵 𝓟𝓪𝓬𝓴𝓪𝓰𝓮𝓼 𝓑𝔂 ██ 𝐄𝐑𝐑☢︎𝐑-𝐗-𝐏𝐑☢︎𝐅𝐄𝐒𝐒☢︎𝐑⁩ ██ "
+
+exit
